67b2da6f1239a4caf9d0c3b9009a8abb486a5164
deepakhosur6 <deepakh.xworkz@gmail.com>
2022-11-28 19:59:29 +0530

    adding file

---
+++ opp/Inheritance/beach/src/com/xworkz/beachapp/BeachesTest.java
@@ -0,0 +1,10 @@
+package com.xworkz.beachapp;
+
+public class BeachesTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ opp/Inheritance/beach/src/com/xworkz/beachapp/beach/Beaches.java
@@ -0,0 +1,17 @@
+;
+
+public class Beaches {
+
+	//Data Transfer Object -- Design pattern
+	private int id ;
+	private String name ;
+	
+	public int getId() {
+		return id;
+
+	}
+	public void setId (int id) {
+		this.id=id;
+	}
+
+}
